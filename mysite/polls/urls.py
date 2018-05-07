3882315588471138de40902592230cfa4ff67689
angstyteen <38361320+angstyteen@users.noreply.github.com>
2018-05-07 13:36:15 -0500

    master

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-
+from django.http import HttpResponse
 from . import views
+from .models import Question
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
